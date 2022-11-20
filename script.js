c2da2805849929ed8e58861b6ea68364264ca17c
Dmitriy Angve <kirirch129@gmail.com>
2022-11-20 09:07:27 +0300

    #

---
+++ script.js
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////////////
+//////////////Exporting and Importing in ES6 Modules/////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+console.log('Importing module');
+
+// addToCart('bread', 5);
+// console.log(price, tq);
+// console.log(shippingCost); // ReferenceError
+
+// All the exports from the module at the same time (ShoppingCart - "S" with capital letter, like Class)
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5); // 5 bread added to cart
+// console.log(ShoppingCart.totalPrice); // 237 (from shoppimgCart.js)
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price); // 237
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2); // 2 pizza added to cart
+add('bread', 5); // 2 pizza added to cart
+add('oil', 2); // 2 pizza added to cart
+add('apples', 2); // 2 pizza added to cart
+
+console.log(cart);
---
+++ shoppingCart.js
@@ -0,0 +1,21 @@
+// Exporting module
+console.log('Exporting module');
+
+const shippingCost = 10; // Can used only here
+export const cart = []; // all to-level variable is a private for this MODULE
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+// Default exports use for only want to export one thing per module
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
